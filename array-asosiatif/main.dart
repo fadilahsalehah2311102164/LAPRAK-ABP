511107d95cff937fcdafa6464907de38808647c1
fadilahsalehah2311102164 <salehahfadilah2005@gmail.com>
2026-04-06 20:06:37 +0700

    Add files via upload

---
+++ array-asosiatif/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  var mahasiswa = {
+    "nama": "Fadilah",
+    "umur": 21,
+    "jurusan": "informatika"
+  };
+
+  print(mahasiswa["nama"]);
+  print(mahasiswa["umur"]);
+  print(mahasiswa["jurusan"]);
+}
